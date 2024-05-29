3e6735380c2810ba1b9e9c98278aee48943eb664
Andre Luis <andreluis333@hotmail.com.br>
2024-05-29 14:57:10 -0400

    commit3,add calc de emprestimo

--- segundoprojeto.py
+++ segundoprojeto.py
@@ -1,3 +1,11 @@
 a = 5
 b = 3
 print(a+b)
+
+# calculo para financiamento ou emprestimo
+Renda=float(input('Renda Mensal: '))
+Valor_Desejado=float(input('Qual valor desejado: '))
+if Renda>=5000 and Valor_Desejado<=0.2*Renda:
+    print('(Seu pedido foi Aprovado)')
+else:
+    print('(Seu pedido não foi Aprovado)')    
