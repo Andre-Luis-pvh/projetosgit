d78bbab2af6dfd2f365d667ad2163a7c97f3e611
Andre Luis <andreluis333@hotmail.com.br>
2024-05-29 09:53:09 -0400

    commit2

--- primeiroprojetopython.py
+++ primeiroprojetopython.py
@@ -1,8 +1,13 @@
 y = 10
 print(y)
 
-m = float(input('Digite sua nota aqui: '))
+m = float(input('coloque sua nota aqui: '))
 if m >= 7:
     print('- Aprovado,Parabéns.')
 else:
     print('- Reprovado,Estude Mais.')    
+
+idadeana = 32
+idadejulia = 19
+y = idadeana > idadejulia
+print(y)
